74323b96211bc475fdfad11e6f1f8072b5920af3
brunopc-net <work@brunopc.net>
2023-03-22 11:53:21 -0400

    Temporarily removing selenium execution for jenkins testing

--- src/amazon.py
+++ src/amazon.py
@@ -7,18 +7,20 @@ logger = log4p.GetLogger(__name__, config="log4p.json").logger
 
 PRICE_XPATH = '//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span/span[1]'
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-serv = Service(r"C:\Selenium\chromedriver.exe")
-driver = webdriver.Chrome(service=serv, options=chrome_options)
+
+def get_driver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    serv = Service(r"C:\Selenium\chromedriver.exe")
+    return webdriver.Chrome(service=serv, options=chrome_options)
 
 
 def get_price(code):
-    driver.get("https://www.amazon.ca/dp/"+code)
+    driver = get_driver()
+    driver.get("https://www.amazon.ca/dp/" + code)
     current_price = driver.find_element(By.XPATH, PRICE_XPATH).get_attribute('innerHTML').replace('$', '')
     logger.info(f'Current price: {current_price}')
     return current_price
-
 
 # Timer starts
 # start = timeit.default_timer()
